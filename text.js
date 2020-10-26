81b2044d958e86d0a373cd1787c279247251b589
zhangmingyang <zhangmingyang@sogou-inc.com>
2020-10-26 16:31:55 +0800

    上传文件

--- text.js
+++ text.js
@@ -7,10 +7,10 @@ const RTCPeerConnection =
     window.webkitRTCPeerConnection ||
     window.mozRTCPeerConnection;
 
-const callBtn = document.getElementById("callBtn");
-const sendBtn = document.getElementById("send-btn");
-const hangBtn = document.getElementById("hangBtn");
-const contentInput = document.getElementById("contentInput");
+const callBtn = document.getElementById('call-btn');
+const sendBtn = document.getElementById('send-btn');
+const hangBtn = document.getElementById('hang-btn');
+const contentInput = document.getElementById('contentInput');
 
 let pc;
 let mediaConstraints = null;
@@ -21,37 +21,28 @@ let receiveBuffer = [];
 let receivedSize = 0;
 let fileInfo = {
     size: 0,
-    name: ""
+    name: '',
 };
 
 // 监听
-const socket = io("http://10.129.20.203:3000");
+const socket = io('http://10.129.20.203:3000');
 socket.open();
-socket.on("broadcast", msg => {
+socket.on('broadcast', msg => {
     const {
-        data: {sdp, candidate}
+        data: { sdp, candidate },
     } = msg;
     switch (msg.type) {
-        case "offer":
-            tip("接收offer");
+        case 'offer':
+            tip('接收offer');
             remoteDescription = sdp;
             remoteCandidate = candidate;
             createAnswer();
             break;
-        case "answer":
-            tip("接收answer");
+        case 'answer':
+            tip('接收answer');
             remoteDescription = sdp;
             remoteCandidate = candidate;
             finishConnection();
-            break;
-        case "fileInfo":
-            const {
-                data: {name, size, filetype, lastmodify}
-            } = msg;
-            fileInfo = {
-                fileSize: size,
-                fileName: name
-            };
             break;
         default:
             return;
@@ -62,82 +53,100 @@ socket.on("broadcast", msg => {
 pc = new RTCPeerConnection(conf, mediaConstraints);
 // 接收p2p消息
 pc.ondatachannel = e => {
-    console.log("传输通道打开");
+    console.log('传输通道打开');
     channel = e.channel;
     channel.onopen = () => {
-        tip("可以 发送/接收 消息");
-        console.log("接收通道打开");
+        tip('可以 发送/接收 消息');
+        console.log('接收通道打开');
     };
     channel.onclose = () => {
-        tip("关闭消息通道");
-        console.log("接收通道关闭");
+        tip('关闭消息通道');
+        console.log('接收通道关闭');
     };
     channel.onmessage = e => {
-        console.log("接收通道信息");
-        if (toString.call(e.data) === "[object ArrayBuffer]") {
-            receiveFile(e);
+        if (toString.call(e.data) === '[object ArrayBuffer]') {
+            handleFile(e);
         } else {
-            receiveText("收到:" + e.data);
+            const data = JSON.parse(e.data);
+            switch (data.type) {
+                case 'string':
+                    receiveText('收到:' + data.text);
+                    break;
+                case 'image/jpeg':
+                    fileInfo = {
+                        fileSize: data.size,
+                        fileName: data.name,
+                        fileType: data.type,
+                    };
+                    break
+                default:
+                    fileInfo = {
+                        fileSize: data.size,
+                        fileName: data.name,
+                        fileType: data.type,
+                    };
+                    console.error(444, data.type);
+            }
         }
     };
 };
 
 //
 pc.onicecandidate = e => {
-    console.log("on ice candidate", e);
+    console.log('on ice candidate', e);
     if (e.candidate) {
         //这里传输candidate给对方
         localCandidate = e.candidate;
         if (!remoteCandidate) {
-            socket.emit("broadcast", {
-                type: "offer",
+            socket.emit('broadcast', {
+                type: 'offer',
                 data: {
                     sdp: localDescription,
-                    candidate: localCandidate
-                }
+                    candidate: localCandidate,
+                },
             });
         }
-        console.error("获取到ice: ", e.candidate);
+        console.error('获取到ice: ', e.candidate);
     }
 };
-pc.onidentityresult = function () {
-    console.log("on identity result");
+pc.onidentityresult = function() {
+    console.log('on identity result');
 };
-pc.onidpassertionerror = function () {
-    console.log("on id passertion error");
+pc.onidpassertionerror = function() {
+    console.log('on id passertion error');
 };
-pc.onidpvalidationerror = function () {
-    console.log("on id pvalidation error");
+pc.onidpvalidationerror = function() {
+    console.log('on id pvalidation error');
 };
 pc.onnegotiationneeded = createOffer;
-pc.onpeeridentity = function () {
-    console.log("on peeridentity");
+pc.onpeeridentity = function() {
+    console.log('on peeridentity');
 };
-pc.onremovestream = function () {
-    console.log("on remove stream");
+pc.onremovestream = function() {
+    console.log('on remove stream');
 };
-pc.onconnectionstatechange = function () {
-    console.log("on connection state change: ", pc.connectionState);
+pc.onconnectionstatechange = function() {
+    console.log('on connection state change: ', pc.connectionState);
 };
-pc.oniceconnectionstatechange = function () {
-    console.log("on ice connection state change: ", pc.iceConnectionState);
+pc.oniceconnectionstatechange = function() {
+    console.log('on ice connection state change: ', pc.iceConnectionState);
 };
-pc.onsignalingstatechange = function () {
-    console.log("on signaling state change: ", pc.signalingState);
+pc.onsignalingstatechange = function() {
+    console.log('on signaling state change: ', pc.signalingState);
 };
-pc.ontrack = function () {
-    console.log("on track");
+pc.ontrack = function() {
+    console.log('on track');
 };
 
 // 创建offer
 function createOffer() {
-    console.error(2, "创建offer", "on negotiation needed");
+    console.error(2, '创建offer', 'on negotiation needed');
     pc.createOffer().then(offer => {
-        console.error(3, "向远端广播offer", offer);
+        console.error(3, '向远端广播offer', offer);
         localDescription = offer;
         //这里传输Description 给接听方 , 手动复制
-        tip("创建offer");
-        console.error(4, "设置本地offer");
+        tip('创建offer');
+        console.error(4, '设置本地offer');
         pc.setLocalDescription(offer);
     });
 }
@@ -147,16 +156,16 @@ function createAnswer() {
     // 1 设置远程description
     pc.setRemoteDescription(new RTCSessionDescription(remoteDescription));
     // 2 创建answer
-    pc.createAnswer().then(function (answer) {
-        console.error(5, "创建远程answer", answer);
+    pc.createAnswer().then(function(answer) {
+        console.error(5, '创建远程answer', answer);
         localDescription = answer;
-        tip("创建answer");
+        tip('创建answer');
         pc.setLocalDescription(answer);
-        socket.emit("broadcast", {
-            type: "answer",
+        socket.emit('broadcast', {
+            type: 'answer',
             data: {
-                sdp: localDescription
-            }
+                sdp: localDescription,
+            },
         });
     });
     // 3 添加ico后补
@@ -169,87 +178,128 @@ function finishConnection() {
 }
 
 //1 请求呼叫
-callBtn.onclick = function () {
-    console.error(1, "创建dataChannel");
-    channel = pc.createDataChannel("hehe", mediaConstraints); //可以发送文字什么的
-    channel.onopen = function () {
-        tip("可以 发送/接收 消息");
-        console.log("hehe通道打开", channel);
+callBtn.onclick = () => {
+    console.error(1, '创建dataChannel');
+    channel = pc.createDataChannel('hehe', mediaConstraints); //可以发送文字什么的
+    channel.onopen = () => {
+        tip('可以 发送/接收 消息');
+        console.log('hehe通道打开', channel);
     };
-    channel.onclose = function () {
-        tip("关闭消息通道");
-        console.log("hehe通道关闭");
+    channel.onclose = () => {
+        tip('关闭消息通道');
+        console.log('hehe通道关闭');
     };
-    channel.onmessage = function (e) {
-        console.log("hehe通道信息");
-        receive("收到:" + e.data);
+    channel.onmessage = e => {
+        // console.log('hehe通道信息');
+        receiveText('收到:' + e.data);
     };
 };
 
 // 发送
-sendBtn.onclick = function () {
-    channel.send(contentInput.value);
+sendBtn.onclick = () => {
+    if (!channel) return;
+    channel.send(
+        JSON.stringify({
+            type: 'string',
+            text: contentInput.value,
+        }),
+    );
     send(contentInput.value);
-    contentInput.value = "";
+    contentInput.value = '';
 };
 
 //关闭
-hangBtn.onclick = function () {
+hangBtn.onclick = () => {
+    if (!channel) return;
     channel.close();
     pc.close();
 };
 
-/***
- *操作提示
- */
+// 操作提示
 function tip(msg) {
-    const d = document.createElement("div");
-    d.className = "tip";
+    const d = document.createElement('div');
+    d.className = 'tip';
     d.innerHTML = msg;
     log.appendChild(d);
 }
 
-/***
- *操作提示
- */
+// 接收文字
 function receiveText(msg) {
-    const d = document.createElement("div");
-    d.className = "receive-text";
+    const d = document.createElement('div');
+    d.className = 'receive-text';
     d.innerHTML = msg;
     log.appendChild(d);
 }
 
+function handleFile(e) {
+    receiveBuffer.push(e.data);
+    receivedSize += e.data.byteLength;
+    receiveProgress.value = receivedSize;
+    const { fileSize, fileName, fileType } = fileInfo;
+
+    if (receivedSize === fileSize) {
+        console.error('------ 文件接收完成 ------')
+        const received = new Blob(receiveBuffer);
+        // 接收完成初始化
+        receiveBuffer = [];
+        receivedSize = 0;
+        const fileData = {
+            href: URL.createObjectURL(received),
+            fileName,
+            fileSize,
+        }
+        if (fileType === 'image/jpeg') {
+            receiveImage(fileData);
+        } else {
+            receiveFile(fileData)
+        }
+    }
+}
+
+// 接收图片
 function receiveImage(info) {
-    const a = document.createElement("a");
-    a.className = "receive-href";
-    a.setAttribute("href", info.href);
+    const a = document.createElement('a');
+    a.className = 'receive-href';
+    a.setAttribute('href', info.href);
     a.download = info.fileName;
-    const img = document.createElement("img");
-    img.className = "receive-img";
-    img.setAttribute("src", info.href);
+
+    const img = document.createElement('img');
+    img.className = 'receive-img';
+    img.setAttribute('src', info.href);
     a.appendChild(img);
     log.appendChild(a);
 }
 
+function receiveFile(info) {
+    const { href, fileName, fileSize } = info;
+    const a = document.createElement('a');
+    a.className = 'receive-href';
+    a.setAttribute('href', href);
+    a.download = fileName;
+    a.innerText = `下载 ${fileName} ${fileSize}`;
+
+    log.appendChild(a);
+}
+
 function send(msg) {
-    const d = document.createElement("div");
-    d.className = "send-text";
+    const d = document.createElement('div');
+    d.className = 'send-text';
     d.innerHTML = msg;
     log.appendChild(d);
 }
 
 /*------ 下载 ------*/
-const fileInput = document.getElementById("fileInput");
-const fileBtn = document.getElementById("file-btn");
-const sendProgress = document.getElementById("sendProgress");
-const statusMessage = document.getElementById("status");
+const fileInput = document.getElementById('fileInput');
+const fileBtn = document.getElementById('file-btn');
+const sendProgress = document.getElementById('sendProgress');
+const statusMessage = document.getElementById('status');
 
-fileInput.addEventListener("change", handleFileInputChange);
+fileInput.addEventListener('change', handleFileInputChange);
 
 function handleFileInputChange() {
     const file = fileInput.files[0];
     if (!file) {
-        console.log("No file chosen");
+        console.log('No file chosen');
     } else {
         sendData();
 
@@ -260,51 +310,49 @@ function handleFileInputChange() {
     }
 }
 
-fileBtn.onclick = () => fileInput.click();
+fileBtn.onclick = () => {
+    if (!channel) return;
+    fileInput.click();
+};
 
 function sendData() {
     let offset = 0;
     let chunkSize = 16384;
     let file = fileInput.files[0];
-    const {name, size, type, lastModified} = file;
-    console.error(`File is ${[name, size, type, lastModified].join(" ")}`);
+    const { name, size, type, lastModified } = file;
 
-    socket.emit("broadcast", {
-        type: "fileInfo",
-        data: {
+    channel.send(
+        JSON.stringify({
             name,
             size,
-            filetype: type,
-            lastModified
-        }
-    });
+            type,
+            lastModified,
+        }),
+    );
 
     // Handle 0 size files.
-    statusMessage.textContent = "";
+    statusMessage.textContent = '';
     if (file.size === 0) {
-        bitrateDiv.innerHTML = "";
-        statusMessage.textContent = "File is empty, please select a non-empty file";
+        bitrateDiv.innerHTML = '';
+        statusMessage.textContent = 'File is empty, please select a non-empty file';
         return;
     }
 
     sendProgress.max = file.size;
 
     fileReader = new FileReader();
-    fileReader.onerror = error => console.error("Error reading file:", error);
-    fileReader.onabort = event => console.log("File reading aborted:", event);
+    fileReader.onerror = error => console.error('Error reading file:', error);
+    fileReader.onabort = event => console.log('File reading aborted:', event);
     fileReader.onload = e => {
-        console.log("FileRead.onload ", e);
-        const {result} = e.target;
+        const { result } = e.target;
         channel.send(result);
         offset += result.byteLength;
         sendProgress.value = offset;
-        if (offset < file.size) {
-            readSlice(offset);
-        }
+        if (offset < file.size) readSlice(offset);
     };
 
     const readSlice = o => {
-        console.log("readSlice ", o);
+        // console.log('readSlice ', o);
         const slice = file.slice(offset, o + chunkSize);
         fileReader.readAsArrayBuffer(slice);
     };
@@ -312,23 +360,4 @@ function sendData() {
     readSlice(0);
 }
 
-function receiveFile(e) {
-    console.error("接收文件");
-    receiveBuffer.push(e.data);
-    receivedSize += e.data.byteLength;
-    receiveProgress.value = receivedSize;
-    const {fileSize, fileName} = fileInfo;
-    if (receivedSize === fileSize) {
-        const received = new Blob(receiveBuffer);
-        receiveBuffer = [];
-        receiveImage({
-            href: URL.createObjectURL(received),
-            fileName
-        });
 
-        // downloadAnchor.href = URL.createObjectURL(received);
-        // downloadAnchor.download = fileName;
-        // downloadAnchor.textContent = `Click to download '${fileName}' (${fileSize} bytes)`;
-        // downloadAnchor.style.display = "block";
-    }
-}
